6365d8066404c9535db0594bec0fb8c47c35999c
Stephanie Leazer <saleazer@gmail.com>
2020-11-25 19:16:03 -0600

    Added grid

--- scrumleaderboard/src/App.vue
+++ scrumleaderboard/src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <GridPractice/><br>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GridPractice from './components/GridPractice'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GridPractice
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Verdana, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  text-align: left;
+  color: #841ff8;
+  margin-top: 15px;
+ 
 }
+
+
+
+
 </style>
---
+++ scrumleaderboard/src/components/BoardItem.vue
@@ -0,0 +1,33 @@
+<template>
+   <v-container>  
+      <div v-if="item != null">
+        <v-card id="card" elevation="4" height=100>
+          <v-card-text>{{item.ID}}:{{item.Title}}</v-card-text>
+        </v-card>
+      </div>
+   </v-container>
+</template>
+<script>
+
+
+export default {
+  name: 'BoardItem',
+  
+  props: {
+    item: {
+      type: Object,
+      default: null
+      }
+  },
+  
+  data(){
+    return {
+    }
+  },
+   
+  mounted(){
+    console.log(this.item);
+  },
+}
+
+</script>
